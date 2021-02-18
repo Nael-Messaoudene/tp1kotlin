a03181ddaaaf0f55b9fce1405c733481d5268344
naelmessaoudene <naelmessaoudene@gmail.com>
2021-02-18 10:46:29 +0100

    add calculator

--- app/src/main/java/com/gmail/nmessaoudene/tp01_messaoudene_nael/MainActivity.java
+++ app/src/main/java/com/gmail/nmessaoudene/tp01_messaoudene_nael/MainActivity.java
@@ -6,16 +6,22 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.gmail.nmessaoudene.tp01_messaoudene_nael.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
 
+    ActivityMainBinding  binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         initViews();
         Log.v("bonjour","test");
 
@@ -24,11 +30,19 @@ public class MainActivity extends AppCompatActivity {
     private Button buttonToast;
     private Button buttonInc;
     private TextView textViewCount;
+    private Button buttonOperation;
+    private TextView resultOperationView;
+    private EditText firstNumber;
+    private EditText secondNumber;
 
     private void initViews() {
-        buttonToast = findViewById(R.id.toast);
-        buttonInc = findViewById(R.id.count);
-        textViewCount = findViewById(R.id.txtView);
+        buttonToast = binding.toast;
+        buttonInc = binding.count;
+        textViewCount = binding.txtView;
+        buttonOperation = binding.buttonOperation;
+        resultOperationView = binding.resultOperationView;
+        firstNumber = binding.firstNumber;
+        secondNumber = binding.secondNumber;
 
         buttonInc.setOnClickListener(new View.OnClickListener() {
 
@@ -40,6 +54,23 @@ public class MainActivity extends AppCompatActivity {
                 Log.v("clique","OKOKOKOKOKOKOKOKOKOK");
             }
         });
+
+        buttonOperation.setOnClickListener(new View.OnClickListener() {
+
+            //nt counter = 0;
+            //int sum = Integer.parseInt(String.valueOf(firstNumber.getText())) + Integer.parseInt(String.valueOf(secondNumber.getText()));
+            @Override
+            public void onClick(View view) {
+                //counter ++;
+                int sum1 = Integer.parseInt(String.valueOf(binding.firstNumber.getText()));
+                int sum2 = Integer.parseInt(String.valueOf(binding.secondNumber.getText()));
+                
+                  Log.v("oparation","firstNumber");
+                resultOperationView.setText(String.valueOf(sum1 + sum2));
+                Log.v("clique","opération !!!!!!");
+            }
+        });
+
 
         buttonToast.setOnClickListener(new View.OnClickListener() {
 
